cd5690b197619626830f77f2860dc9bbd67e14d1
Ian Duncan <duncan.ian.t@gmail.com>
2023-03-09 00:39:05 -0600

    More space to tab fixes

--- scheduled_scripts/repeater-audio.js
+++ scheduled_scripts/repeater-audio.js
@@ -9,47 +9,47 @@ signale.config({displayTimestamp: true, displayDate: true});
 // Streams repeater audio from broadcastify to voice channel
 
 module.exports = {
-    configFile: null,
-    client: null,
-    init: function(client, config) {
-        this.client = client;
-        this.configFile = config;
-        this.audio(this.client.channels.cache.find(ch => ch.name === this.configFile.stream_chan), config.stream_url, 1);
-    },
-    // Post questions
-    audio: function(channel, track, volume) {
-        const connection = joinVoiceChannel({
-            channelId: channel.id,
-            guildId: channel.guildId,
-            adapterCreator: channel.guild.voiceAdapterCreator,
-        });
-        const player = createAudioPlayer();
+	configFile: null,
+	client: null,
+	init: function(client, config) {
+		this.client = client;
+		this.configFile = config;
+		this.audio(this.client.channels.cache.find(ch => ch.name === this.configFile.stream_chan), config.stream_url, 1);
+	},
+	// Post questions
+	audio: function(channel, track, volume) {
+		const connection = joinVoiceChannel({
+			channelId: channel.id,
+			guildId: channel.guildId,
+			adapterCreator: channel.guild.voiceAdapterCreator,
+		});
+		const player = createAudioPlayer();
 
-        resource = createAudioResource(path.join(__dirname, track), { inlineVolume: true });
-        resource.volume.setVolume(volume);
-        connection.subscribe(player);
-        player.play(resource)
-        connection.on(VoiceConnectionStatus.Ready, () => {console.log("ready"); player.play(resource);})
-        connection.on(VoiceConnectionStatus.Disconnected, async (oldState, newState) => {
-            try {
-                console.log("Disconnected.")
-                await Promise.race([
-                    entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
-                    entersState(connection, VoiceConnectionStatus.Connecting, 5_000),
-                ]);
-            } catch (error) {
-                connection.destroy();
-            }
-        });
-        player.on('error', error => {
-            console.error(`Error: ${error.message} with resource ${error.resource.metadata.title}`);
-            player.play(getNextResource());
-        });
-        player.on(AudioPlayerStatus.Playing, () => {
-            console.log('The audio player has started playing!');
-        }); 
-        player.on('idle', () => {
-            connection.destroy();
-        })
-    }
+		resource = createAudioResource(path.join(__dirname, track), { inlineVolume: true });
+		resource.volume.setVolume(volume);
+		connection.subscribe(player);
+		player.play(resource)
+		connection.on(VoiceConnectionStatus.Ready, () => {console.log("ready"); player.play(resource);})
+		connection.on(VoiceConnectionStatus.Disconnected, async (oldState, newState) => {
+			try {
+				console.log("Disconnected.")
+				await Promise.race([
+					entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
+					entersState(connection, VoiceConnectionStatus.Connecting, 5_000),
+				]);
+			} catch (error) {
+				connection.destroy();
+			}
+		});
+		player.on('error', error => {
+			console.error(`Error: ${error.message} with resource ${error.resource.metadata.title}`);
+			player.play(getNextResource());
+		});
+		player.on(AudioPlayerStatus.Playing, () => {
+			console.log('The audio player has started playing!');
+		}); 
+		player.on('idle', () => {
+			connection.destroy();
+		})
+	}
 }
